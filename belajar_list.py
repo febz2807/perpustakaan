f591e47f2f7a6ed9b2695394581cc8ded880318f
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2025-11-07 20:50:37 +0700

    Add files via upload

---
+++ belajar_list.py
@@ -0,0 +1,33 @@
+# Membuat Program menggunakan For-LOOP, List dan Range
+
+banyak = int(input("Berapa banyak data? "))
+
+nama = []
+umur = []
+
+for i in range(0, banyak):
+    print(f"Data ke {i}")
+    input_nama = input("Nama : ")
+    input_umur = int(input("Umur : "))
+
+    nama.append(input_nama)
+    umur.append(input_umur)
+
+for i in range(0, len(nama)):
+    data_nama = nama[i]
+    data_umur = umur[i]
+    print(f"{data_nama} berumur {data_umur} tahun")
+
+
+
+# Belajar While-Loop
+
+data = ""
+
+while data != "x":
+    print("masukan perulangan")
+    data = input("data : ")
+
+for i in range(1, 100):
+    print(i)
+    
