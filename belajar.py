fd1dada3bbec598240973cd74936974fe920defb
Febryanto Nugroho <febryantonugroho2807@gmail.com>
2025-11-07 20:50:58 +0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+nama = "Febryanto Nugroho"
+kota = 'Jakarta'
+alamat = 'Jalan Subur Raya'
+
+nama_depan = "Febryanto" 
+nama_belakang = "Nugroho"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)
+
+
+nama_depan = "Febryanto"
+nama_tengah = "Kaiser"
+nama_belakang = "Nugroho"
+
+sapa = f"halo {nama_depan} {nama_tengah} {nama_belakang}"
+
+print(sapa)
+
+print("Silahkan masukkan nama : ")
+nama = input ()
+
+print(f"hello {nama}, Selamat Datang")
